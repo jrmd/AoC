610010c5182b0f005a1a59e102ff234346ffcb5b
Jerome Duncan <hello@itsjero.me>
2024-12-03 08:00:18 +0000

    day_01: Add solution for pt 1 and 2

--- day_01/solution.go
+++ day_01/solution.go
@@ -1,30 +1,104 @@
-package template
+package day_01
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 
-func ParseFile(lines []string) ([]string, error) {
-	return lines, nil
+	"github.com/jrmd/AoC/utils"
+)
+
+type IntSlice []int
+
+func (s IntSlice) Len() int {
+   return len(s)
 }
 
-func PartOne() int {
-	return 0
+func (s IntSlice) Swap(i, j int) {
+   s[i], s[j] = s[j], s[i]
 }
 
-func PartTwo() int {
-	return 0
+func (s IntSlice) Less(i, j int) bool {
+   return s[i] > s[j]
+}
+
+func Distance(a, b int) int {
+   if a < b {
+      return b - a
+   }
+   return a - b
+}
+
+func ParseFile(lines []string) (IntSlice, IntSlice, error) {
+  leftSide := IntSlice{}
+  rightSide := IntSlice{}
+
+  for _, line := range lines {
+    vals := strings.Split(line, "   ");
+
+    lhi, err := strconv.Atoi(vals[0])
+    rhi, err := strconv.Atoi(vals[1])
+		if err != nil {
+      panic(err)
+		}
+
+    leftSide = append(leftSide, lhi)
+    rightSide = append(rightSide, rhi)
+  }
+
+  sort.Sort(leftSide)
+  sort.Sort(rightSide)
+
+	return leftSide, rightSide, nil
+}
+
+func PartOne(leftSide, rightSide IntSlice) int {
+  total := 0
+  for i := range leftSide {
+    total += Distance(leftSide[i], rightSide[i])
+  }
+	return total;
+}
+
+func PartTwo(lhs, rhs IntSlice) int {
+  rhsMap := map[int]int{}
+  for _, val := range rhs {
+    _, ok := rhsMap[val];
+
+    if !ok {
+      rhsMap[val] = 1
+      continue;
+    }
+
+    rhsMap[val] += 1
+  }
+
+  total := 0;
+
+  for _, val := range lhs {
+    occurences, ok := rhsMap[val];
+
+    if !ok {
+      continue;
+    }
+
+    total += val * occurences;
+  }
+	return total
 }
 
 func Run() {
-	// lines, err := utils.GetLines("./input.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	lines, err := utils.GetLines("./day_01/input.txt")
+	if err != nil {
+		panic(err)
+	}
 
-	// result, err := ParseFile(lines)
-	// if err != nil {
-	// 	panic(err)
-	// }
+  leftSide, rightSide, err := ParseFile(lines)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Printf("Part One: %d\n", PartOne())
-	fmt.Printf("Part Two: %d\n", PartTwo())
+	fmt.Printf("Part One: %d\n", PartOne(leftSide, rightSide))
+	fmt.Printf("Part Two: %d\n", PartTwo(leftSide, rightSide))
 }
--- day_01/solution_test.go
+++ day_01/solution_test.go
@@ -1,21 +1,39 @@
-package template
+package day_01
 
 import (
 	"testing"
+
+	"github.com/jrmd/AoC/utils"
 )
 
-func TestPart1(t *testing.T) {
-	result := PartOne()
+func GetInput(file string) (IntSlice, IntSlice) {
+	lines, err := utils.GetLines(file)
+	if err != nil {
+		panic(err)
+	}
 
-	if result != 0 {
-		t.Fatalf("Expected 0, got %d", result)
+	lhs, rhs, err := ParseFile(lines)
+	if err != nil {
+		panic(err)
+	}
+
+	return lhs, rhs
+}
+
+func TestPart1(t *testing.T) {
+  lhs, rhs := GetInput("./test.txt")
+	result := PartOne(lhs, rhs)
+
+	if result != 11 {
+		t.Fatalf("Expected 11, got %d", result)
 	}
 }
 
 func TestPart2(t *testing.T) {
-	result := PartTwo()
+  lhs, rhs := GetInput("./test.txt")
+	result := PartTwo(lhs, rhs)
 
-	if result != 0 {
-		t.Fatalf("Expected 0, got %d", result)
+	if result != 31 {
+		t.Fatalf("Expected 31, got %d", result)
 	}
 }
